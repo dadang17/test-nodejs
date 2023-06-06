906f14c8085ffed260debdead5ee66880acfcafc
dadang17 <iskandardzulqornain38@gmail.com>
2023-06-07 06:36:37 +0700

    add cors

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.use(bodyParser.json());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(allRoutes);
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log("server running on port " + PORT);
